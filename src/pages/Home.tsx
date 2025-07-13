f70149b61ff792f1ddf9cf538e5781e024edabfb
Nyambe Imasiku <525792@student.fontys.nl>
2025-07-13 02:51:44 +0200

    yes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom'
 import Search from '../components/SearchBar'
 import { ArrowRight } from 'lucide-react'
 import NavBar from '../components/NavBar'
-import promoImage from '../images/promo_image.png'
 
 
 export default function Home() {
@@ -41,7 +40,7 @@ export default function Home() {
             </div>
             <div className="w-full md:w-1/2 flex justify-center">
               <img
-                src={promoImage}
+                src="src/images/promo_image.png"
                 alt="Trimly Stylists"
                 className="h-64 md:h-80 object-contain shadow-xl"
               />
